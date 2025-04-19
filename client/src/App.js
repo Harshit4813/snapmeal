a041f93efb7178a2e1558d5b3349b29ae94714e5
Harshit Singh <Harshitsingh4281@gmail.com>
2025-04-19 15:16:45 +0530

    feat: connect real UI to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-
 function App() {
   return (
     <div className="App">
@@ -21,5 +20,6 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
