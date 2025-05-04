d24d9ba7c22ba9d53d2e06b8e512861d3cb8157c
Harshit Singh <Harshitsingh4281@gmail.com>
2025-05-04 15:42:08 +0530

    Add HomePage component

---
+++ client/src/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to SnapMeal!</h1>
+      <p>This is your platform's homepage.</p>
+    </div>
+  );
+}
+
+export default HomePage;
